56da7ecf931cf3f0be137131210c0858efa955d1
dersenn <67318888+dersenn@users.noreply.github.com>
2023-02-20 20:35:44 +0100

    variation in Y added.

--- mesh-01/sketch.js
+++ mesh-01/sketch.js
@@ -25,16 +25,21 @@ const nCols = 5
 const nRows = 5
 const nSub = 10
 
+
+let sY = divLength(nVec(0, 0), nVec(0, svg.h), nRows, true, 'RND')
+let eY = divLength(nVec(svg.w, 0), nVec(svg.w, svg.h), nRows, true, 'RND')
+
+
+
+
 let pts = []
 
 for (let y = 0; y <= nRows; y++) {
   pts[y] = divLength(
-    nVec(0, y * (svg.h / nRows)), nVec(svg.w, y * (svg.h / nRows)),
+    sY[y], eY[y],
     nCols, true, 'RND'
   )
 }
-
-let startYs = divLength()
 
 
 let paths = []
